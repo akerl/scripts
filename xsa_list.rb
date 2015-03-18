b328e8d4a270f791832a373a5f21ef1dfc0c536c
Les Aker <me@lesaker.org>
2015-03-17 22:49:10 -0400

    add xsa_list.rb

---
+++ xsa_list.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+
+require 'open-uri'
+require 'nokogiri'
+
+URL = 'http://xenbits.xen.org/xsa/'
+XPATH = '/html/body/table/tr'
+
+class XSA
+  def initialize(raw)
+    @data = raw.xpath('td').map { |y| y.children.map(&:text) }
+  end
+
+  def id
+    @id ||= @data[0].first.split('-').last.to_i
+  end
+
+  def release_dt
+    @release_dt ||= DateTime.parse(@data[1].first)
+  end
+
+  def update_dt
+    return false if @update_dt == false
+    @update_dt ||= DateTime.parse(@data[2].first)
+  rescue TypeError
+    @update_dt = false
+  end
+
+  def version
+    @version ||= @data[3].first.to_i
+  end
+
+  def cves
+    @cves ||= @data[4]
+  end
+
+  def title
+    @title ||= @data[5].first
+  end
+end
+
+LIST = Nokogiri::HTML(open(URL)).xpath(XPATH)[1..-1].map { |x| XSA.new(x) }
+
+LIST.each do |xsa|
+  puts [
+    xsa.id.to_s.rjust(3),
+    xsa.release_dt.to_date,
+    xsa.update_dt ? xsa.update_dt.to_date : ' ' * 10,
+    xsa.version,
+    xsa.title
+  ].join(' | ')
+end

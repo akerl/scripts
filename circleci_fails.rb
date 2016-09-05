87c8d4e044886d6afc3657e637013b6f1b8a7008
Les Aker <me@lesaker.org>
2016-09-05 15:34:24 -0400

    way smarter circle fails script

--- circleci_fails.rb
+++ circleci_fails.rb
@@ -3,23 +3,47 @@
 require 'open-uri'
 require 'json'
 require 'keylime'
+require 'parallel'
 
 ENDPOINT = ENV['CIRCLECI_ENDPOINT'] || 'https://circleci.com'
 
-credential = Keylime.new(server: ENDPOINT)
-token = credential.get!("CircleCI Token (#{ENDPOINT}/account/api)").password
-
-raw = open("#{ENDPOINT}/api/v1/projects?circle-token=#{token}")
-json = JSON.load(raw)
-
-repos = json.map do |x|
-  url = x['vcs_url'].sub('github.com', 'circleci.com/gh')
-  status = x['branches']['master']['recent_builds'].first['status']
-  [url, status]
+def token
+  return @token if @token
+  credential = Keylime.new(server: ENDPOINT)
+  @token = credential.get!("CircleCI Token (#{ENDPOINT}/account/api)").password
 end
-repos.reject! { |_, state| ['success', 'fixed'].include? state }
-repos.sort_by!(&:first)
 
-url_width = repos.map(&:first).map(&:size).max
+def api_url(path)
+  "#{ENDPOINT}/api/v1.1/#{path}?circle-token=#{token}"
+end
 
-repos.each { |url, state| puts "#{url.ljust(url_width)} #{state}" }
+def api_req(path)
+  JSON.load(open(api_url(path)))
+end
+
+def all_projects
+  api_req('projects').map do |project|
+    "#{project['username']}/#{project['reponame']}"
+  end
+end
+
+def status(project)
+  build = api_req("project/github/#{project}").first
+  build ? build['status'] : 'new'
+end
+
+def all_statuses
+  Parallel.map(all_projects) { |x| [x, status(x)] }.to_h
+end
+
+def failing_projects
+  all_statuses.reject { |_, v| %w(success fixed running).include? v }
+end
+
+def print_report!
+  list = failing_projects.to_a.sort_by(&:first)
+  url_width = list.map(&:first).map(&:size).max
+  list.each { |url, state| puts "#{url.ljust(url_width)} #{state}" }
+end
+
+print_report!
